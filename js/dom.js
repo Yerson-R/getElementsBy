620cf8adaaac8783a12b7c8fa6e7d3a8ed9bd0ba
Yerson Rodríguez <yerson2004rt@gmail.com>
2021-10-27 18:04:48 -0500

    getElementsBy

---
+++ js/dom.js
@@ -0,0 +1,28 @@
+//DOM
+//Document Object Model
+//Modelo de Objeto del documento
+/*console.log(document.getElementById("TituloNombre"))
+console.log(document.getElementById("nombre"))
+console.log(document.getElementById("TituloApellidos"))
+console.log(document.getElementById("apellidos"))
+console.log(document.getElementById("TituloTelefono"))
+console.log(document.getElementById("telefono"))
+console.log(document.getElementById("TituloCorreo"))
+console.log(document.getElementById("correo"))*/
+/*console.log(document.getElementsByName("nombre"))
+console.log(document.getElementsByName("apellidos"))
+console.log(document.getElementsByName("telefono"))
+console.log(document.getElementsByName("correo"))*/
+
+
+//console.log(document.getElementsByClassName("clase"));
+//console.log(document.getElementsByTagName("input"));
+console.log(document.getElementsByTagName("form"))
+console.log(document.getElementsByTagName("input"))
+console.log(document.getElementsByTagName("label"))
+console.log(document.getElementsByTagName("br"))
+console.log(document.getElementsByTagName("script"))
+
+
+
+
